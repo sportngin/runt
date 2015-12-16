80018812ac9d158f685f7201580a0465ae88c2c3
Andy Fleener <andrew.fleener@sportngin.com>
2015-12-16 13:52:59 -0600

    Fix broken tests to get all tests passing on 1.8.7

--- test/runttest.rb
+++ test/runttest.rb
@@ -6,11 +6,11 @@ require 'date'
 require 'pp'
 
 class RuntModuleTest < Test::Unit::TestCase
-  
+
   def test_last
     assert Runt::Last == -1
   end
-  
+
   def test_last_of
     assert Runt::Last_of == -1
   end
@@ -36,7 +36,7 @@ class RuntModuleTest < Test::Unit::TestCase
       i=i+1
     end
   end
-  
+
   def test_month_name
     i=0
     Date::MONTHNAMES.each do |n|
@@ -48,29 +48,6 @@ class RuntModuleTest < Test::Unit::TestCase
   def test_strftime
     d=DateTime.new(2006,2,26,14,45)
     assert_equal '02:45PM', Runt.format_time(d)
-  end
-
-  def test_numeric_class_additions
-    assert_equal 0.000001, 1.microsecond
-    assert_equal 0.000001, 1.microseconds
-    assert_equal 0.001, 1.millisecond
-    assert_equal 0.001, 1.milliseconds
-    assert_equal 7, 7.second
-    assert_equal 7, 7.seconds
-    assert_equal 60, 1.minute
-    assert_equal 60, 1.minutes
-    assert_equal 3600, 1.hour
-    assert_equal 3600, 1.hours
-    assert_equal 86400, 1.day
-    assert_equal 86400, 1.days
-    assert_equal 604800, 1.week
-    assert_equal 604800, 1.weeks
-    assert_equal 2592000, 1.month
-    assert_equal 2592000, 1.months
-    assert_equal 31536000, 1.year
-    assert_equal 31536000, 1.years
-    assert_equal 315360000, 1.decade
-    assert_equal 315360000, 1.decades
   end
 
   def test_time_class_dprecision
